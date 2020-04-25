aab26f8ede73640f0847e91f625956c8b3f60d0e
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2020-04-24 21:58:55 -0500

    Update Employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -1,28 +1,3 @@
-//****************************************************************************************************
-//
-//              File:                                   Employee.cpp
-//		
-//              Student:                                Kyle McColgan
-//
-//              Assignment:                             Assignment #6
-//
-//              Course Name:                            Principles of Programming Languages
-// 
-//              Course Number:                          COSC 3810 - 01
-//
-//              Due:                                    April 22nd, 2020
-//
-//
-//              This file contains the parent class implementations.
-//
-//              Other files required:
-//                      1. ProductionWorker.h - Child class declaration.
-//                      2. Employee.h - Parent class declaration.
-//                      3. ProductionWorker.cpp - child class implementation.
-//						4. Driver.cpp - application program driver class.
-//		 
-//		
-//****************************************************************************************************
 
 #include <iostream>
 using namespace std;
