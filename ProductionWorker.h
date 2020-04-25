ed74101698446e3e3ec3d8fb9de0bf2bdea4ccd7
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2020-04-24 21:59:38 -0500

    Update ProductionWorker.h

--- ProductionWorker.h
+++ ProductionWorker.h
@@ -1,28 +1,3 @@
-//****************************************************************************************************
-//
-//              File:                                   ProductionWorker.h
-//		
-//              Student:                                Kyle McColgan
-//
-//              Assignment:                             Assignment #6
-//
-//              Course Name:                            Principles of Programming Languages
-// 
-//              Course Number:                          COSC 3810 - 01
-//
-//              Due:                                    April 22nd, 2020
-//
-//
-//              This file contains the child class declaration, inherits from Employee.
-//
-//              Other files required:
-//                      1. Driver.cpp - application program driver class.
-//                      2. Employee.h - Parent class declaration.
-//                      3. ProductionWorker.cpp - child class implementation.
-//						4. Employee.cpp - parent class implementation.
-//		 
-//		
-//****************************************************************************************************
 
 #ifndef PRODUCTIONWORKER_H
 #define PRODUCTIONWORKER_H
