4c260843509a65501c1e9ea8880192f16e15e287
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2021-02-22 23:59:27 -0600

    Update packetCapture.cpp

--- packetCapture.cpp
+++ packetCapture.cpp
@@ -1,6 +1,6 @@
 //*******************************************************************************
 //    Name: Kyle McColgan
-//    Date: 22 February 2021
+//    Date: 23 February 2021
 //    File name: packetCapture.cpp
 //
 //   Description: This application program displays a list of network hosts.
@@ -25,61 +25,20 @@ void startSniffing ( );
 
 int main ( )
 {
-    const int ERROR_BUFFER_SIZE = 81;
-    struct bpf_program firstFilter;
-    char expression [ ] = "port 23";
-    bpf_u_int32 mask;
-    bpf_u_int32 subnet;
-    struct pcap_pkthdr firstHeader;
-    const u_char * firstPacket;
+    const int EXIT_FLAG = -1;
+    int userChoice = 0;
 
-    char * networkDevice;
-    char errorMsg [ERROR_BUFFER_SIZE];
-
-    pcap_t * sessionHandle;
-
-    pcap_if_t * alldevs;
-    pcap_if_t ** alldevs_ptr = &alldevs;
-
-    if ( pcap_findalldevs ( alldevs_ptr, errorMsg ) == -1 )
+    do
     {
-        cout << "Error finding network interface: " << errorMsg << endl;
-    }
-    else
-    {
-        networkDevice = (* alldevs_ptr)->name;
-    }
+        cout << "Enter \'-1\' to quit: ";
+        cin >> userChoice;
+
+        startSniffing ( );
+        displayHosts ( );
+    } 
+    while ( userChoice != -1 );
     
-    if ( networkDevice == nullptr )
-    {
-        cout << "Error: " << errorMsg << endl;
-    }
-    else
-    {
-        sessionHandle = pcap_open_live ( networkDevice, ERROR_BUFFER_SIZE, 1,
-                                     1000, errorMsg );
-    }
-    
-    if ( sessionHandle == nullptr ) 
-    {
-        cout << "Error: " << errorMsg << endl;
-    }
-
-    if ( pcap_compile(sessionHandle, &firstFilter, expression, 0, subnet ) == -1)
-    {
-        cout << "Error: pcap compile" << endl;
-    }
-
-    if ( pcap_setfilter ( sessionHandle, &firstFilter ) == -1 )
-    {
-        cout << "Error: pcap filter" << endl;
-    }
-
-    firstPacket = pcap_next ( sessionHandle, &firstHeader );
-
-    pcap_loop ( sessionHandle, 0, handlePackets, nullptr );
-
-    pcap_close ( sessionHandle );
+    cout << "Goodbye." << endl;
 
     return 0;
 }
@@ -114,7 +73,63 @@ void displayHosts ( )
 
 void startSniffing ( )
 {
+    const int ERROR_BUFFER_SIZE = 81;
+    struct bpf_program firstFilter;
+    char expression[] = "port 23";
+    bpf_u_int32 mask;
+    bpf_u_int32 subnet;
+    struct pcap_pkthdr firstHeader;
+    const u_char *firstPacket;
+
+    char *networkDevice;
+    char errorMsg[ERROR_BUFFER_SIZE];
+
+    pcap_t *sessionHandle;
+
+    pcap_if_t *alldevs;
+    pcap_if_t **alldevs_ptr = &alldevs;
+
     cout << "Begin listening..." << endl;
+
+    if (pcap_findalldevs(alldevs_ptr, errorMsg) == -1)
+    {
+        cout << "Error finding network interface: " << errorMsg << endl;
+    }
+    else
+    {
+        networkDevice = (*alldevs_ptr)->name;
+    }
+
+    if (networkDevice == nullptr)
+    {
+        cout << "Error: " << errorMsg << endl;
+    }
+    else
+    {
+        sessionHandle = pcap_open_live(networkDevice, ERROR_BUFFER_SIZE, 1,
+                                       1000, errorMsg);
+    }
+
+    if (sessionHandle == nullptr)
+    {
+        cout << "Error: " << errorMsg << endl;
+    }
+
+    if (pcap_compile(sessionHandle, &firstFilter, expression, 0, subnet) == -1)
+    {
+        cout << "Error: pcap compile" << endl;
+    }
+
+    if (pcap_setfilter(sessionHandle, &firstFilter) == -1)
+    {
+        cout << "Error: pcap filter" << endl;
+    }
+
+    firstPacket = pcap_next(sessionHandle, &firstHeader);
+
+    pcap_loop(sessionHandle, 0, handlePackets, nullptr);
+
+    pcap_close(sessionHandle);
 
     cout << "Ending listening..." << endl;
 }
