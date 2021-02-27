6bd27f1f504debce737b639d8fe4911fc8fd8cca
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2021-02-26 23:59:07 -0600

    Update packetCapture.cpp

--- packetCapture.cpp
+++ packetCapture.cpp
@@ -1,9 +1,11 @@
 //*******************************************************************************
+//
 //    Name: Kyle McColgan
-//    Date: 26 February 2021
+//    Date: 27 February 2021
 //    File name: packetCapture.cpp
 //
-//   Description: This application program displays a list of network hosts.
+//   Description: This application program uses the pcap network API to display 
+//                a list of network hosts.
 //
 //*******************************************************************************
 
@@ -17,6 +19,7 @@ void beginProcessing ( );
 void displayPacket ( const u_char * packet, struct pcap_pkthdr header );
 void handlePackets (u_char * args, const struct pcap_pkthdr * header, 
                     const u_char * body );
+
 void displayHosts ( );
 void startSniffing ( );
 
@@ -31,7 +34,7 @@ int main ( )
 
 //*******************************************************************************
 
-void beginProcesseing ( )
+void beginProcessing ( )
 {
     const int EXIT_FLAG = -1;
     int userChoice = 0;
@@ -80,18 +83,14 @@ void displayHosts ( )
 void startSniffing ( )
 {
     const int ERROR_BUFFER_SIZE = 81;
+
     struct bpf_program firstFilter;
     char expression[] = "port 23";
-    bpf_u_int32 mask;
     bpf_u_int32 subnet;
     struct pcap_pkthdr firstHeader;
-    const u_char *firstPacket;
-
     char *networkDevice;
     char errorMsg[ERROR_BUFFER_SIZE];
-
     pcap_t *sessionHandle;
-
     pcap_if_t *alldevs;
     pcap_if_t **alldevs_ptr = &alldevs;
 
@@ -130,8 +129,6 @@ void startSniffing ( )
     {
         cout << "Error: pcap filter" << endl;
     }
-
-    firstPacket = pcap_next(sessionHandle, &firstHeader);
 
     pcap_loop(sessionHandle, 0, handlePackets, nullptr);
 
